259281790a1c804f9b6ef396ef1b142e55cb2976
lorisCarignano <loris.carignano@gmail.com>
2022-09-19 12:26:34 +0200

    modifiche

--- 001_chatbroadcastUDP/client.py
+++ 001_chatbroadcastUDP/client.py
@@ -1,10 +1,25 @@
 from socket import AF_INET, SOCK_DGRAM, socket
+from server import PORT
+
+utente=str(input("inserisci il nome utente: "))
+INDIRIZZO=str(input("inserisci il tuo indirizzo: "))
+
 
 def chatclient():
+    
+
+    
     with  socket(AF_INET, SOCK_DGRAM) as s:
-        msg= "carig"
-        msg = msg.encode()
-        s.sendto(msg, ("192.168.95.255",5000))
+        while True:
+            msg= input("chat: ")
+            messaggio=utente+": "+msg
+            messaggio = messaggio.encode()
+            s.sendto(messaggio, (INDIRIZZO,PORT))
+
+
+    
+  
+
 
 if __name__ == "__main__":
     chatclient()
